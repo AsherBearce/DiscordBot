8ddb3a4ab4a20976602fb5080c0d5a05374254ea
AsherBearce <49050298+AsherBearce@users.noreply.github.com>
2019-09-15 00:50:29 -0600

    Now writes valid token to a file upon first time set up, or upon a failed login attempt.

--- src/main/java/io/github/asherbeace/bot/TableBot.java
+++ src/main/java/io/github/asherbeace/bot/TableBot.java
@@ -13,6 +13,7 @@ import net.dv8tion.jda.api.hooks.ListenerAdapter;
 
 import javax.annotation.Nonnull;
 import javax.security.auth.login.LoginException;
+import java.io.*;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
@@ -21,21 +22,52 @@ public class TableBot {
     public static JDA jda;
     public static final String PREFIX = "!t";
     public static final long TIME_COMMAND_REFRESH = 300000;
-    //TODO Add a file write to keep the bot token. 
+    public static final String FILE_NAME = "botToken.txt";
     //TODO Add the ability to ban members from using the commands for a short time. Use a List<User> for this.
     //TODO Set up multi-stage commands.
 
-    public static void main(String[] args) throws LoginException {
-        Scanner input = new Scanner(System.in);
-        System.out.println("Please enter the token of your bot: ");
-        String s = input.nextLine();
-
-        //Put token here
-        jda = new JDABuilder(AccountType.BOT).setToken(s).build();
+    public static void main(String[] args){
+        File tokenFile = new File(FILE_NAME);
+        if (tokenFile.exists()){
+            try {
+                BufferedReader reader = new BufferedReader(new FileReader(tokenFile));
+                String token = reader.readLine();
+                jda = new JDABuilder(AccountType.BOT).setToken(token).build();
+            } catch (Exception e){
+                System.out.println("There was a problem retrieving your bots token! Performing first-time setup.");
+                firstTimeSetup();
+            }
+        } else {
+            firstTimeSetup();
+        }
 
         jda.addEventListener(new EventHandler());
 
+    }
 
+    private static void firstTimeSetup(){
+        boolean loginSuccess = false;
+
+        while (!loginSuccess) {
+            Scanner input = new Scanner(System.in);
+            System.out.println("Please enter the valid token of your bot: ");
+            String s = input.nextLine();
+
+            try {
+                //Put token here
+                jda = new JDABuilder(AccountType.BOT).setToken(s).build();
+                loginSuccess = true;
+
+                BufferedWriter writer = new BufferedWriter(new FileWriter(FILE_NAME));
+                writer.write(s);
+
+                writer.close();
+            } catch (LoginException e) {
+                System.out.println("An error occurred while logging in. It may be that the token entered was invalid.");
+            } catch (IOException e){
+                System.out.println("There was an error storing your bots token. Please retry.");
+            }
+        }
     }
 
     public static void parseCommand(GuildMessageReceivedEvent event){
