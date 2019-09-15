32dd99ea48975d07331ab63b120644a7ae0d7d12
AsherBearce <49050298+AsherBearce@users.noreply.github.com>
2019-09-14 23:00:59 -0600

    Add simple prompt to enter the bot token for security purposes.

--- src/main/java/io/github/asherbeace/bot/TableBot.java
+++ src/main/java/io/github/asherbeace/bot/TableBot.java
@@ -5,23 +5,37 @@ import net.dv8tion.jda.api.AccountType;
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
 import net.dv8tion.jda.api.entities.TextChannel;
+import net.dv8tion.jda.api.entities.User;
+import net.dv8tion.jda.api.events.guild.GuildLeaveEvent;
+import net.dv8tion.jda.api.events.guild.member.GuildMemberLeaveEvent;
 import net.dv8tion.jda.api.events.message.guild.GuildMessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 
+import javax.annotation.Nonnull;
 import javax.security.auth.login.LoginException;
 import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
 
 public class TableBot {
     public static JDA jda;
     public static final String PREFIX = "!t";
     public static final long TIME_COMMAND_REFRESH = 300000;
+    //TODO Add a file write to keep the bot token. 
+    //TODO Add the ability to ban members from using the commands for a short time. Use a List<User> for this.
+    //TODO Set up multi-stage commands.
 
     public static void main(String[] args) throws LoginException {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Please enter the token of your bot: ");
+        String s = input.nextLine();
+
         //Put token here
-        jda = new JDABuilder(AccountType.BOT).setToken("")
-                .build();
+        jda = new JDABuilder(AccountType.BOT).setToken(s).build();
 
         jda.addEventListener(new EventHandler());
+
+
     }
 
     public static void parseCommand(GuildMessageReceivedEvent event){
@@ -65,6 +79,11 @@ public class TableBot {
             if (!event.getAuthor().getId().equalsIgnoreCase("135829757324558336")){
                 parseCommand(event);
             }
+        }
+
+        public void onGuildMemberLeave(GuildMemberLeaveEvent event){
+            String name = event.getUser().getName();
+            event.getGuild().getDefaultChannel().sendMessage("Oh no! " + name + " has left the server!").queue();
         }
     }
 }
